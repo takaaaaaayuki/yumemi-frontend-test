714db9cad2d9777fbd091c9f1066e1f1e58da010
takaaaaaayuki <s2322045@stu.musashino-u.ac.jp>
2025-06-01 02:56:30 +0900

    feat: Homeページに都道府県セレクターを表示

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
+// src/pages/Home.tsx
 import React from 'react';
 import PrefectureSelector from '../components/PrefectureSelector';
 
-const Home = () => (
-  <div>
-    <h1>都道府県別人口推移</h1>
-    <PrefectureSelector />
-  </div>
-);
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>都道府県別人口推移</h1>
+      <PrefectureSelector />
+    </div>
+  );
+};
 
 export default Home;
